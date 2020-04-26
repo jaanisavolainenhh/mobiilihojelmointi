35e19d172ddb81f789dde5f623b4f7abdb1ecec5
Jaani Savolainen <bgh273@myy.haaga-helia.fi>
2020-04-26 10:53:24 +0300

    tehtava 9

---
+++ tehtava9/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Tehtava from './components/tehtava9'
+export default function App() {
+  return (
+    <Tehtava />
+  );
+}
+
